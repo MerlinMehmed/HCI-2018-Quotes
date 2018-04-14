e1c979f10579d5f208c10c80d2f00bb64d799afb
Elena Zarkova <ezarkova@gmail.com>
2018-04-14 19:31:20 +0300

    Buttons working in firefox

--- quotes/src/views/viewAuthors.js
+++ quotes/src/views/viewAuthors.js
@@ -8,19 +8,19 @@ class viewAuthors extends Component {
 			<div className="container col-lg-12 components">
 				<div className="col-sm-2 buttons">
 					<div className="row">
-						<button className="col-sm-12 button" type="button">
+						<div className="col-sm-12 button">
 							<Link to="/Browse/Authors" className="link" activeClassName="active">Автори</Link>
-						</button>
+						</div>
 					</div>
 					<div className="row">
-						<button className="col-sm-12 button" type="button">
+						<div className="col-sm-12 button">
 							<Link to="/Browse/Books and magazines" className="link" activeClassName="active">Книги и списания</Link>
-						</button>	
+						</div>	
 					</div>
 					<div className="row">
-						<button className="col-sm-12 button" type="button">
+						<div className="col-sm-12 button">
 							<Link to="/Browse/Categories" className="link" activeClassName="active">Категории</Link>
-						</button>	
+						</div>	
 					</div>
 				</div>
 				<div className="col-sm-10 view" id="authors">
